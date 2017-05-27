82438713b72b38d542b21c9b1368b76d9bc7fbb5
MeksPwnschlager <doctoranyk@gmail.com>
2017-05-27 18:54:21 +0200

    add rainbow effet

--- effects.py
+++ effects.py
@@ -40,3 +40,26 @@ class LineScrollEffect:
 
     def set_speed(self, v):
         self.speed = 0.5 + v * 40
+
+class RainbowEffect:
+    def __init__(self):
+        self.speed = 1
+        self.offs = 1
+        self.fb = UmbrellaFB()
+
+    def update_and_get_fb(self, ts):
+        s = ts * self.speed
+
+        for x in range(0,3):
+            for y in range(0,4):
+               pos = (s + self.offs * y) % 1
+               c = hsv_to_rgb(pos, 1, 255)
+               self.fb.draw(x, y, int(c[0]), int(c[1]), int(c[2]),0,0)
+
+        return self.fb
+
+    def set_speed(self, v):
+       self.speed = v % 1
+    
+    def set_offset(self, v):
+       self.offs = v % 0.3
